72426483e92d614cd161b0453a78172580cee8f3
Matheus <matheustosros@gmail.com>
2025-10-02 00:29:48 -0300

    testando configuração de commit

--- App-KnowHall/App.tsx
+++ App-KnowHall/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tela de Login</Text>
+      <Text style={styles.title}>Login</Text>
 
       <TextInput
         style={styles.input}
